c91d89b3281503a15fdeeb21ce0b40bc03193d87
fr-me-jim <fr.me.jim@gmail.com>
2024-03-12 09:14:51 +0100

    fix: image load

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-	matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+	matcher: "/((?!api|_next/static|_next/image|.png|favicon.ico).*)",
 };
 
 // This function can be marked `async` if using `await` inside
